afa95a845137827ea2f0086197a6f0b366c231cc
doaw <519561524@qq.com>
2024-03-18 18:14:46 +0800

    setting&&router&&nav

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup lang="ts">
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+ <template>
+  <div>
+    <!-- <h1>Welcome to Vue 3 + Vue Router App</h1> -->
+    
+    <router-view/>
+  </div>
+</template>
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav class="navbar">
+      <router-link to="/" class="navbar-brand">
+        <img src="../../public/bomb.png" alt="Logo" class="logo"/>
+      </router-link>
+      <div class="navbar-end">
+        <router-link to="/">
+          <img src="../../public/bomb.png" alt="Profile" class="icon"/>
+        </router-link>
+      </div>
+    </nav>
+  </template>
+  
+  <script setup lang="ts">
+  // TypeScript script (if needed)
+  </script>
+  
+  <style scoped>
+  .navbar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 60px;
+    width: 100%;
+    background-color: #e27a11;
+    justify-content: space-between; /* 这会把子元素分到两边 */
+  align-items: center; /* 对齐项目到中心线 */
+  padding: 0 20px; /* 按需调整内边距 */
+    
+  }
+  .logo {
+    position: absolute;
+    height: 50px;
+    left: 0px /* Adjust based on your logo's size */
+  }
+  .icon {
+    position: absolute;
+    height: 30px; /* Adjust based on your icon's size */
+    right: 40px;
+    
+  }
+  </style>
