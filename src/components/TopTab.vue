7eabdb1402c764a6b2afbf17bf866c97769c2338
summerxixi <2235709694@qq.com>
2024-03-25 21:51:25 +0800

    增加pinia，完成个人信息和诗词飞花令大部分

--- src/components/TopTab.vue
+++ src/components/TopTab.vue
@@ -1,84 +1,117 @@
 <template>
   <div class="tabbox">
-    <div class="active">
-      <!-- <img src="@/assets/imgs/gabtn1.png" alt="">
-      <img src="@/assets/imgs/gabtn2.png" alt=""> -->
-      自主游玩</div>
-    <!-- <div 
-      :style="{ backgroundColor: currentIndex === 0 ? bgColor : '', color: fontColor }" 
-      class="btn" 
-      @click="handleLeftClick"
-    >
+    <div  v-if="currentIndex === 0"  class="btn" :class="{ active: currentIndex === 0 }" 
+    :style="{backgroundColor:bgColor,color: fontColor}"   @click="handleLeftClick1">
+      <img src="@/assets/imgs/gabtn1.png" class="imgleft" alt="">
+      <img src="@/assets/imgs/gabtn2.png" class="imgright" alt="">
       {{ leftContent }}
     </div>
-    <div 
-      :style="{ backgroundColor: currentIndex === 1 ? bgColor : '', color: fontColor }" 
-      class="btn" 
-      @click="handleRightClick"
-    >
-      {{  rightContent  }}
-    </div> -->
+
+    <div class="btn" v-if="currentIndex === 1" 
+      @click="handleLeftClick2">
+      {{ leftContent }}
+    </div>
+    
+    <div  v-if="currentIndex === 1"  class="btn" :class="{ active: currentIndex === 1 }" 
+    :style="{backgroundColor:bgColor,color: fontColor}">
+      <img src="@/assets/imgs/gabtn1.png" class="imgleft" alt="">
+      <img src="@/assets/imgs/gabtn2.png" class="imgright" alt="">
+      {{ rightContent }}
+    </div>
+    <div class="btn" v-if="currentIndex === 0" 
+      @click="handleRightClick1">
+      {{ rightContent }}
+    </div>
 
   </div>
 </template>
 
 <script setup lang="ts">
-// import { onMounted, ref, defineProps } from 'vue';
+import { onMounted, ref, watch,defineProps,defineEmits } from 'vue';
+import {useStateStore}  from '@/store/index.ts'
+const emits = defineEmits(['update:currentIndex']); // 定义需要向父组件发送的自定义事件
+const props = defineProps(['foo', 'bgColor', 'fontColor', 'currentIndex']);
+const currentIndex = ref(0);
+const bgColor = ref(props.bgColor);
+const fontColor = ref(props.fontColor);
+const leftContent = "自主游玩"
+const rightContent = "匹配游玩"
+const store = useStateStore();
+watch(currentIndex, (newVal) => {
+  console.log("emit")
+    emits('update:currentIndex', newVal); // 向父组件发送自定义事件，并传递新的 currentIndex 值
+  });
+  
+const handleLeftClick1 = () => {
+  currentIndex.value = 0; // 切换为左边
+  store.changeBeginState("开始游玩")
+};
 
-// const props = defineProps(['foo', 'bgColor', 'fontColor','currentIndex']);
+const handleLeftClick2 = () => {
+  currentIndex.value = 0; // 切换为左边
+  store.changeBeginState("开始游玩")
+  store.changeFlywordState(0)
+};
 
-// const currentIndex = ref(props.currentIndex);
-// const bgColor = ref(props.bgColor); 
-// const fontColor = ref(props.fontColor); 
-// const leftContent = "自主游玩"
-// const rightContent ="匹配游玩"
-
-// const handleLeftClick = () => {
-//   currentIndex.value = 0; // 切换为左边
-// };
-
-// const handleRightClick = () => {
-//   currentIndex.value = 1; // 切换为右边
-// };
+const handleRightClick1 = () => {
+  currentIndex.value = 1; // 切换为右边
+  store.changeBeginState("开始匹配")
+  store.changeFlywordState(1)
+};
+const handleRightClick2 = () => {
+  currentIndex.value = 1; // 切换为右边
+};
 
 
-// onMounted(() => {
-//   currentIndex.value = props.currentIndex;
-//   console.log(props);
-// });
+onMounted(() => {
+  // currentIndex.value = props.currentIndex;
+  console.log(props);
+});
 </script>
 
 <style scoped>
-.active{
-  border: 3rem solid rgba(236, 196, 135, 1);
-  border-radius: 15rem;
-
-width: 280rem;
-height: 93rem;
-opacity: 1;
-
-}
-/* .tabbox{
+.tabbox{
+  width: 598rem;
   display: flex;
   justify-content: space-between;
-  gap: 50rem; /* 两个按钮之间的间距为 50rem */
+  gap: 50rem;
+}
+.active {
+  border: 3rem solid rgba(236, 196, 135, 1);
+  border-radius: 21rem;
+  position: relative;
+  width: 280rem;
+  height: 93rem;
+
+  .imgleft {
+    position: absolute;
+    left: -70rem;
+    bottom: -38rem;
+    z-index: 99;
+  }
+
+  .imgright {
+    position: absolute;
+    left: 169rem;
+    bottom: -17rem;
+    z-index: 99;
+  }
+
+}
+
+
 
 
 .btn {
-  width: 271.86rem;
-  height: 92rem;
+  width: 276.86rem;
+  height: 83.39rem;
   flex: 1;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: auto 0;
-  opacity: 1;
-  border-radius: 10rem;
   font-size: 36rem;
   font-weight: 400;
-  letter-spacing: 0rem;
   line-height: 52.13rem;
-  color: rgba(107, 0, 0, 1);
-  cursor: pointer; /* 添加鼠标指针样式，表明可点击 */
-} 
+  cursor: pointer;
+}
 </style>
